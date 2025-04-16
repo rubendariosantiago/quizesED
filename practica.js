4d3026a9408e8db8133005a8e3991cec866ed88f
rubendariosantiago <ruben.dario@itesm.mx>
2025-04-16 15:59:12 -0600

    Update practica.js

--- practica.js
+++ practica.js
@@ -1,43 +1,34 @@
 const practicalTemplates = [
-  {
-    type: "positivo",
-    template: "Resuelve: \\( y' + {{a}}y = {{b}} \\), \\( y(0) = {{d}} \\)",
-    solution: "\\frac{ {{b}} }{ {{a}} } + \\left( {{d}} - \\frac{ {{b}} }{ {{a}} } \\right) e^{-{{a}}x}",
-    conditions: ["b != 0"],
+    {
+    type: "lineal_constante",
+    template: "Resuelve: \\( y' + {{a}}y = {{b}} \\) con \\( y(0) = {{c}} \\)",
+    solution_mathjs: "{{b/a}} + ({{c}} - {{b/a}}) * exp(-{{a}}*x)",
+    solution_latex: "{{b/a}} + \\left( {{c -b/a}} \\right) e^{-{{a}}x}",
     params: {
-      a: {min: 2, max: 5},
-      b: {min: -4, max: 4},
-      d: {min: -3, max: 3}
-    }
+      a: { min: 2, max: 5 },
+      b: { min: 2, max: 5 },
+      c: { min: -3, max: 3 }
+    },
+    conditions: ["a != 0"]
   },
   {
-    type: "negativo",
-    template: "Resuelve: \\( y' - {{a}}y = {{b}}e^{ {{c}}x } \\), \\( y(0) = {{d}} \\)",
-    solution: "\\frac{ {{b}} }{ {{c}}-{{a}} } e^{ {{c}}x } + \\left( {{d}} - \\frac{ {{b}} }{ {{c}}-{{a}} } \\right) e^{ {{a}}x }",
-    conditions: ["c != a"],
+    type: "trigonometrica_coseno",
+    template: "Resuelve: \\( y' + {{a}}y = {{b}} \\cos({{c}}x) \\) con \\( y(0) = {{d}} \\)",
+    solution_mathjs: "({{a*b}}*cos({{c}}*x) + {{b*c}}*sin({{c}}*x))/{{a^2 + c^2}} + ({{d}} - {{a*b/(a^2 + c^2)}}) * exp(-{{a}}*x)",
+    solution_latex: "\\frac{ {{a*b}} \\cos({{c}}x) + {{b*c}} \\sin({{c}}x) }{ {{a^2 + c^2}} } + \\left( {{d}} - \\frac{ {{a*b}} }{ {{a^2 + c^2}} } \\right) e^{-{{a}}x}",
     params: {
-      a: {min: 2, max: 4},
-      b: {min: 2, max: 4},
-      c: {min: 2, max: 4},
-      d: {min: -3, max: 3}
+      a: { min: 2, max: 5 },
+      b: { min: 2, max: 5 },
+      c: { min: 2, max: 5 },
+      d: { min: -3, max: 3 }
     }
   },
   {
     type: "exponencial_positivo",
-    template: "Resuelve: \\( y' + {{a}}y = {{b}}e^{ {{c}}x } \\), \\( y(0) = {{d}} \\)",
-    solution: "\\frac{ {{b}} }{ {{c}}+{{a}} } e^{ {{c}}x } + \\left( {{d}} - \\frac{ {{b}} }{ {{c}}+{{a}} } \\right) e^{-{{a}}x}",
+    template: "Resuelve: \\( y' + {{a}}y = {{b}}e^{ {{c}}x } \\) con \\( y(0) = {{d}} \\)",
+    solution_mathjs: "({{b/(c+a)}}*exp({{c}}*x) + {{d-b/(c+a) }}*exp({-{a}}*x))",
+    solution_latex: "\\frac{ {{b}} }{ {{c}}+{{a}} } e^{ {{c}}x } + \\left( {{d}} - \\frac{ {{b}} }{ {{c}}+{{a}} } \\right) e^{-{{a}}x}",
     conditions: ["c != -a"],
-    params: {
-      a: {min: 2, max: 4},
-      b: {min: 2, max: 4},
-      c: {min: 2, max: 4},
-      d: {min: -3, max: 3}
-    }
-  },
-  {
-    type: "trigonometrico_coseno",
-    template: "Resuelve: \\( y' + {{a}}y = {{b}} \\cos( {{c}}x ) \\), \\( y(0) = {{d}} \\)",
-    solution: "\\frac{ {{a}}^2 {{d}} e^{-{{a}} x} + {{a}} {{b}} \\cos( {{c}} x) - {{a}} {{b}} e^{-{{a}} x} + {{c}}^2 {{d}} e^{-{{a}} x} + {{b}} {{c}} \\sin( {{c}} x) }{ {{a}}^2 + {{c}}^2 }",
     params: {
       a: {min: 2, max: 4},
       b: {min: 2, max: 4},
