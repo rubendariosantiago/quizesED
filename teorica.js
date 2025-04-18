5b8f4926f0e9ca0899b70668bb4774fdac21a8b4
rubendariosantiago <ruben.dario@itesm.mx>
2025-04-18 01:50:01 -0600

    Update teorica.js

--- teorica.js
+++ teorica.js
@@ -8,10 +8,21 @@ const theoryQuestions = [
       "\\( e^{Q(x)} \\)"
     ],
     answer: 0,
-    solution: "El factor integrante se define como \\( \\mu(x) = e^{\\int P(x)dx} \\).  Al multiplicar ambos lados de la ecuación diferencial original la transforma en exacta."
+    solution: "El factor integrante se define como \\( \\mu(x) = e^{\\int P(x)dx} \\).  La ecuación diferencial original se transforma en exacta al multiplicarla por el factor integrante."
+  },
+    {
+    question: "Determina el factor integrante de la ecuación diferencial lineal $$ y' + 2xy = 4x^3 $$",
+    options: [
+      "\\( \\mu(x)= e^{x^2} \\)",
+      "\\( \\mu(x)= x^4\\)",
+      "\\( \\mu(x)= 8x^4 \\)",
+      "\\( \\mu(x)= e^{2x} \\)"
+    ],
+    answer: 0,
+    solution: "El factor integrante es \\( \\mu(x) = e^{\\int P(x)dx}=  e^{\\int 2xdx}=  e^{x^2}  \\).  La ecuación diferencial original se transforma en exacta al multiplicarla por el factor integrante."
   },
   {
-    question: "La solución general de \\(\\displaystyle\\{ \\frac{dy}{dx}  - 3y = 0  \\} \\) es:",
+    question: "La solución general de \\(\\displaystyle{ \\frac{dy}{dx}  - 3y = 0  } \\) es:",
     options: [
       "\\( y = Ce^{3x} \\)",
       "\\( y = Ce^{-3x} \\)",
@@ -19,7 +30,28 @@ const theoryQuestions = [
       "\\( y = Cx^3 \\)"
     ],
     answer: 0,
-    solution: "La ecuación diferencial es separable: \\[ \\frac{dy}{y} = 3dx \\longrightarrow \\ln|y| = 3x + C \\longrightarrow y = Ce^{3x} \\]"
+    solution: "La ecuación diferencial es separable: \\[ \\frac{dy}{y} = 3dx \\Longrightarrow \\ln|y| = 3x + C \\Longrightarrow y = Ce^{3x} \\]"
   },
-  // Resto de las preguntas...
+    {
+    question: "Determina la ecuación diferencial de primer porden que es lineal",
+    options: [
+      "\\( \\displaystyle{ y\\frac{dy}{dx} + 2y = e^{3x} }\\)",
+      "\\( \\displaystyle{ \\frac{dy}{dx} + 2x^2y = xe^{3x} } \\)",
+      "\\( \\displaystyle{ \\frac{dy}{dx} + 2y = e^{3y} }\\)",
+      "\\( \\displaystyle{ y\\frac{dy}{dx} + x^2y^2 = e^{3x} } \\)"
+    ],
+    answer: 1,
+    solution: "La forma de una ecuación diferencial lineal de primer orden es:  \\[ \\frac{dy}{y}+P(x)y=Q(x)  \\]"
+  },
+{
+    question: "La solución general de \\(\\displaystyle{ \\frac{dy}{dx}  +4y = 0  } \\) es:",
+    options: [
+      "\\( y = Ce^{-4x} \\)",
+      "\\( y = Ce^{4x} \\)",
+      "\\( y = C + e^{4x} \\)",
+      "\\( y = Cx^4 \\)"
+    ],
+    answer: 0,
+    solution: "La ecuación diferencial es separable: \\[ \\frac{dy}{y} = -4dx \\Longrightarrow \\ln|y| = -4x + C \\Longrightarrow y = Ce^{-4x} \\]"
+  },
 ];
